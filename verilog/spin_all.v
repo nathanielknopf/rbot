3c58d2079221af00394b1d005ff9852b06e64fee
Nathaniel Knopf <nathanielknopf@gmail.com>
2017-11-07 16:05:04 -0500

    spin_all started

---
+++ verilog/spin_all.v
@@ -0,0 +1,7 @@
+// basically need an internal variable that tracks what block of moves to spit out
+// can have a memory module that stores a shit ton of strings of moves
+// some dumb nerd shit
+
+module spin_all(input send_setup_moves, clock, 
+                output reg [n:0] moves);
+endmodule
