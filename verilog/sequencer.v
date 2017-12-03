ec76131dd962708827b5b77ae01e350662fdcb5b
Jacob Swiezy <jeswiezy@mit.edu>
2017-12-03 17:35:46 -0500

    Sequencer module prototype finished.

--- verilog/sequencer.v
+++ verilog/sequencer.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Jacob Swiezy
 // 
 // Create Date: 12/01/2017 02:42:22 PM
 // Design Name: 
 // Module Name: sequencer
-// Project Name: 
+// Project Name: rbot
 // Target Devices: 
 // Tool Versions: 
 // Description: 
@@ -20,16 +20,65 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module sequencer #(parameter NUM_STEPS=0)
+module sequencer
     (input clock,
-    input start_seq,
-    input [(NUM_STEPS*3)-1:0] sequence,
-    output seq_done,
-    output [3:0] next_move,
-    output start_move,
+    input seq_complete,
+    input new_moves,
+    input [199:0] seq,
+    output reg seq_done,
+    output reg [3:0] next_move,
+    output reg start_move,
     input move_done
     );
     
+    localparam IDLE = 0;
+    localparam ADD_TO_QUEUE = 1;
+    localparam LOAD_MOVE = 2;
+    localparam WAIT_FOR_MOVE = 3;
+    localparam SEQ_FINISHED = 4;
     
+    reg [2:0] state = IDLE;
+    reg [199:0] part_seq;
+    reg [3:0] moves [7:0];
+    reg [7:0] curr_step = 0;
+    reg [7:0] num_moves = 0;
+    
+    always @(posedge clock)begin
+        case(state)
+            IDLE: begin
+                seq_done <= 0;
+                if(new_moves) begin
+                    part_seq[199:0] <= seq[199:0];
+                    state <= ADD_TO_QUEUE;
+                end else if(seq_complete) begin
+                    state <= LOAD_MOVE;
+                end
+            end
+            ADD_TO_QUEUE: begin
+                moves[num_moves] <= part_seq[199:196];
+                num_moves <= (|part_seq[199:196]) ? num_moves + 1 : num_moves;
+                part_seq <= part_seq << 4;
+                state <= (|(part_seq[195:0])) ? ADD_TO_QUEUE : IDLE;
+            end
+            LOAD_MOVE: begin
+                next_move <= moves[curr_step];
+                curr_step <= curr_step + 1;
+                start_move <= 1;
+                state <= WAIT_FOR_MOVE;
+            end
+            WAIT_FOR_MOVE: begin
+                start_move <= 0;
+                if(move_done) begin
+                    state <= (curr_step >= num_moves) ? LOAD_MOVE : SEQ_FINISHED;
+                end
+            end
+            SEQ_FINISHED: begin
+                seq_done <= 1;
+                curr_step <= 0;
+                num_moves <= 0;
+                state <= IDLE;
+            end
+        endcase
+    end
     
 endmodule
