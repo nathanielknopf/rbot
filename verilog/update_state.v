4c229ea808bd4093b32cf97d5cbd97690151001a
Nathaniel Knopf <nathanielknopf@gmail.com>
2017-12-03 21:58:21 -0500

    outline of update_state.v

--- verilog/update_state.v
+++ verilog/update_state.v
@@ -1,6 +1,6 @@
 module update_state (
     input clock, [199:0] moves, new_moves_ready, [161:0] cubestate,
-    output reg[161:0] cubestate_updated, 
+    output reg[161:0] cubestate_updated, reg state_updated
 );
     // moves
     parameter R = 4'd2;
@@ -16,11 +16,59 @@ module update_state (
     parameter D = 4'd12;
     parameter Di = 4'd13;
 
-    
     parameter IDLE = 0;
     parameter MOVING = 1;
 
+    reg state = IDLE;
+
+    reg [5:0] counter = 0;
+
     always @(posedge clock) begin
+        case (state)
+            MOVING: begin
+                case (next_move):
+                    R: begin
+                    end
+                    Ri: begin
+                    end
+                    U: begin
+                    end
+                    Ui: begin
+                    end
+                    F: begin
+                    end
+                    Fi: begin
+                    end
+                    L: begin
+                    end
+                    Li: begin
+                    end
+                    B: begin
+                    end
+                    Bi: begin
+                    end
+                    D: begin
+                    end
+                    Di: begin
+                    end
+                endcase
+                counter <= counter + 1;
+                moves <= moves << 4
+                next_move <= moves[195:192]
+                // if we've just done our 50th move (counter == 49) then go to IDLE
+                state <= (counter < 49) ? MOVING : IDLE
+                state_updated <= (counter == 49) ? 1 : 0
+            end
+            IDLE: begin
+                counter <= 0; // always be ready...
+                state_updated <= 0;
+                if (new_moves_ready) begin
+                    next_move <= moves[199:196];
+                    state <= MOVING;
+                end
+            end
+            default : state <= IDLE;
+        endcase
     end
 
 endmodule
